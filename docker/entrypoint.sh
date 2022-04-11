1fc0bda9cde44725bbe4bda50e5106e4e2124cfb
Joseph Suhudu <joseph@ingenious.or.ke>
2022-04-11 22:12:04 +0300

    suspend automation in docker/entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,10 +7,10 @@ GID=${GID:-1000}
 usermod -o -u "$UID" user1
 groupmod -o -g "$GID" user1
 
-# exec su user1
+exec su user1
 
-poetry install --no-dev --no-root
+# poetry install --no-dev --no-root
 
-poetry run python manage.py migrate
+# poetry run python manage.py migrate
 
-poetry run python manage.py runserver 0.0.0.0:8000
+# poetry run python manage.py runserver 0.0.0.0:8000
