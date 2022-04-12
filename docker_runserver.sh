a31672c82852136618d3f85f527ba45cf718c7d3
Joseph Suhudu <joseph@ingenious.or.ke>
2022-04-12 18:46:07 +0300

    add https surpport on django dev server

--- docker_runserver.sh
+++ docker_runserver.sh
@@ -3,3 +3,7 @@ poetry install --no-dev --no-root
 poetry run python manage.py migrate
 
 poetry run python manage.py runserver 0.0.0.0:8000
+
+# TODO https://django-extensions.readthedocs.io/en/latest/runserver_plus.html#ssl
+# poetry run python manage.py runserver_plus 0.0.0.0:8000 --cert-file fullchain.pem --key-file privkey.pem # https://timonweb.com/django/https-django-development-server-ssl-certificate/
+poetry run python manage.py runserver_plus 0.0.0.0:8000
