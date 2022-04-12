084e39ace5284faae0f1ad7f577f3639d4b99616
Joseph Suhudu <joseph@ingenious.or.ke>
2022-04-12 17:09:34 +0300

    enable cors for dev instance

--- ie_ecosystem/settings.py
+++ ie_ecosystem/settings.py
@@ -239,7 +239,8 @@ CORS_ALLOWED_ORIGINS = [
 #     "http://127.0.0.1:9000",
     "http://127.0.0.1:3000",
     "http://localhost:3000",
-    "http://localhost:8000"
+    "http://localhost:8000",
+    "http://client.ingenious.or.ke:5059",
 ]
 
 
