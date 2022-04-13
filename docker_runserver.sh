016f4ee92fb8b23af6bb71b9a94078582c6a063f
Joseph Suhudu <joseph@ingenious.or.ke>
2022-04-13 15:19:16 +0300

    discontinue https on django dev server

--- docker_runserver.sh
+++ docker_runserver.sh
@@ -2,10 +2,10 @@ poetry install --no-dev --no-root
 
 poetry run python manage.py migrate
 
-# poetry run python manage.py runserver 0.0.0.0:8000
+poetry run python manage.py runserver 0.0.0.0:8000
 
 # TODO https://django-extensions.readthedocs.io/en/latest/runserver_plus.html#getting-started
 # poetry run python manage.py runserver_plus 0.0.0.0:8000
 # TODO https://django-extensions.readthedocs.io/en/latest/runserver_plus.html#ssl
-poetry run python manage.py runserver_plus 0.0.0.0:8000 --cert-file cert.crt
+# poetry run python manage.py runserver_plus 0.0.0.0:8000 --cert-file cert.crt
 # poetry run python manage.py runserver_plus 0.0.0.0:8000 --cert-file fullchain.pem --key-file privkey.pem # https://timonweb.com/django/https-django-development-server-ssl-certificate/
