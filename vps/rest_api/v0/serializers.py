f4b113f43b2a1cdde180a38874a8a903f9eef95c
Joseph <ingeniousdevke@gmail.com>
2022-10-03 01:04:16 +0300

    filters and api shape for citizen syncing

--- vps/rest_api/v0/serializers.py
+++ vps/rest_api/v0/serializers.py
@@ -135,7 +135,7 @@ class OccurrenceReadSerializer(BaseModelSerializer):
     class Meta(BaseModelSerializer.Meta):
         model = Occurrence
         fields  = ["id", "ob_no", "narrative", "location", "police_station", "police_officer", "module", "is_closed",
-                    "reporters", "unregistered_reporters", "details", 'is_complete'
+                    "reporters", "unregistered_reporters", "details", 'is_complete',
                     "posted_date"] # TODO https://www.django-rest-framework.org/api-guide/relations/#reverse-relations
         depth = 2
 
