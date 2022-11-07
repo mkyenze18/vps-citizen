773a0eb79a157197e333cfb25b99bbb0bc8636b2
Joseph <ingeniousdevke@gmail.com>
2022-11-08 00:50:33 +0300

    make warrants-arrestee m2m

--- vps/rest_api/v0/serializers.py
+++ vps/rest_api/v0/serializers.py
@@ -216,7 +216,6 @@ class ArresteeReadSerializer(BaseModelSerializer):
     """
     """
     # TODO https://www.django-rest-framework.org/api-guide/relations/#example
-    warrants = WarrantofarrestSerializer(many=True, read_only=True)
     accomplices = AccompliceSerializer(many=True, read_only=True)
     next_of_kins = NextofkinSerializer(many=True, read_only=True)
     # mugshots = MugShotsSerializer(many=True, read_only=True)
